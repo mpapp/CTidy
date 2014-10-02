3b5fa61f39431ee8f13c3d6f5c58b1af39b01ce1
mz2 <matias.piipari@gmail.com>
2014-10-02 14:20:33 +0100

    Adds an OSX framework target for tidy.

---
+++ CTidyOSX/CTidyOSX.h
@@ -0,0 +1,19 @@
+//
+//  CTidyOSX.h
+//  CTidyOSX
+//
+//  Created by Matias Piipari on 02/10/2014.
+//  Copyright (c) 2014 Ignition Soft Limited. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CTidyOSX.
+FOUNDATION_EXPORT double CTidyOSXVersionNumber;
+
+//! Project version string for CTidyOSX.
+FOUNDATION_EXPORT const unsigned char CTidyOSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CTidyOSX/PublicHeader.h>
+
+
